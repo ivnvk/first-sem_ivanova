8c81a79fb8baca470b9f08992e72654e5bc8ad0c
ivnvk <94236942+ivnvk@users.noreply.github.com>
2022-11-01 01:41:38 +0300

    Create 579A.cpp

---
+++ ivanova_k_a/579A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int n,ans;
+    while (std::cin>>n)
+    {
+        k=0;
+        while (n>1){
+            if (n%2==0)
+                n=n/2;
+            else{
+                n=n-1;
+                k++;
+                }
+        }
+        std::cout<<ansk
+    return 0;
+}
