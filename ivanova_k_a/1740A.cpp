74265c970c2d43e1b736ee00d058548d5a8ec3a9
ivnvk <94236942+ivnvk@users.noreply.github.com>
2022-11-01 18:32:05 +0300

    Create 1740A.cpp

---
+++ ivanova_k_a/1740A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+ 
+int main(){
+    int t;
+    std::cin>>t;
+    for(int i=0; i<t; i++){
+        int n;
+        std::cin>>n;
+    if (n==2){
+        std::cout<<"7"<<std::endl;
+    }
+    else{
+        std::cout<<"3"<<std::endl;}
+    }    
+    return 0;
+}
