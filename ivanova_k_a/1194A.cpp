0e63b63279bf931b990dc53f79bbb2d830ef9fd2
ivnvk <94236942+ivnvk@users.noreply.github.com>
2022-11-01 18:27:21 +0300

    Create 1194A.cpp

---
+++ ivanova_k_a/1194A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+int main() {
+        long long t;
+        std::cin>>t;
+        while(t--) {
+            long long n,x;
+            std::cin>>n>>x;
+            std::cout<<2*x<<std::endl;
+        }
+    return 0;
+}
