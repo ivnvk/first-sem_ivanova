f17aeaa9b6496d23c5dc963dc25966ceaac796c1
ivnvk <94236942+ivnvk@users.noreply.github.com>
2022-11-08 11:29:24 +0300

    Create 1017A_ch.cpp

---
+++ ivanova_k_a/1017A_ch.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+int n, x, t;
+int a = -1;
+int main() {
+	std::cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int c = 0;
+		for (int j = 1; j = 4; j++) {
+			std::cin >> x;
+			c += x;
+		}
+		if (c > a) {
+			a = c;
+			t = i;
+		}
+	}
+	std::cout << t;
+	return 0;
+}
