69b06a89ff1ea4960e89e58b8f813245b663ce90
ivnvk <94236942+ivnvk@users.noreply.github.com>
2022-12-20 12:04:22 +0300

    Create bpm_22_4-3.cpp

---
+++ ivanova_k_a/bpm_22_4-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    std::string a1 = "number";
+    while (a1.find("n"))
+    {
+        a1.erase(a.find("n"), 1);
+    }
+    std::cout << a1 << std::endl;
+}
+
+##################################
+
+#include <iostream>
+#include <string>
+int main()
+{
+    std::string a1 = "cest";
+    std::string a2 = "westir";
+    a = [];
+    b = [];
+    c = [];
+    
+    
+    while (a.find("n"))
+    {
+        a.erase(a.find("n"), 1);
+    }
+    std::cout << a << std::endl;
+}
+
+
+
+// идея 
+ввод - 2 строки
+создание 3 пустых массивов, разделяем введенные строки на
