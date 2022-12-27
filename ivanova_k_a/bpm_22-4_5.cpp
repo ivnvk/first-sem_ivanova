b1044b39c70747c01e37987286ce956a4eff0685
ivnvk <94236942+ivnvk@users.noreply.github.com>
2022-12-27 12:09:16 +0300

    Create bpm_22-4_5.cpp

---
+++ ivanova_k_a/bpm_22-4_5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+    int c;
+    std::cin >> c;
+    while (c != 0) {
+        int n;
+        std::cin >> n;
+        if (n > 50) {
+            int k;
+            k = n / 10;
+            int ln;
+            int fn;
+            ln = (n*10 - k*100) / 10;
+            fn = k;
+            //std::cout << ln;
+            //std::cout << fn;
+            //std::cout << ' ' ;
+            int m;
+            m = ln*10 + fn;
+            std::cout << m;
+            std::cout << ' ' ;
+            
+        }
+
+        c -= 1;
+        
+    }
+
+}
+
+
+
+
+//Решение для двуначных чисел, нет смены
